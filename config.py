5bf0e97b718284712250dcb6c1f5eb2d59b36442
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-05-02 14:42:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
+import os
 api_id = "24473318"
 api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+bot_token = os.environ.get("bot_token", "") 
+auth_users = [6556141430]
